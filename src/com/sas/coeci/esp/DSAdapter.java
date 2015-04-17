8c599dcc8ad4815e09b202af878de127e57a5fe8
khanab85 <khaled@nabli.de>
2015-04-17 11:12:14 +0200

    Version 0.8
    Change-Id: I86cb7410c00029b101ac8d1b57f8b16fdf12c0c3
    Signed-off-by: khanab85 <khaled@nabli.de>

--- src/com/sas/coeci/esp/DSAdapter.java
+++ src/com/sas/coeci/esp/DSAdapter.java
@@ -1,7 +1,11 @@
 package com.sas.coeci.esp;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -17,70 +21,62 @@ import com.sas.esp.api.pubsub.dfESPclientHandler;
 public class DSAdapter {
 
 	static private boolean nonBusyWait = true;
-	static private String rtdmHost = "sasbap.demo.sas.com";
-	static private int rtdmPort = 8680;
-	static private String espWindowUrl = "dfESP://localhost:55555/project/contQuery/twitterEvent?snapshot=true";
-	static private String rtdmEventName = "ESP_Twitter_Event";
 	
+	private static Properties readConfiguration(String filename) throws IOException {
+		
+		Properties configProps = new Properties();
+		InputStream inputStream = new FileInputStream(new File(filename));
+		configProps.load(inputStream);
+		
+		return configProps;
+		
+	}
 
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-	public static void main(String[] args) throws IOException {
-
-		String engineUrl = espWindowUrl;
-		
-		Configurator.fromFile(new File("esp-rtdm-adapter.properties")).activate();
-		
-		Logger.info("Start Logging");
+	public static void main(String[] args) {
+		Logger.info("Starting Adapter");
+		Properties configProps;
+		try {
+			Configurator.fromFile(new File("esp-rtdm-adapter.properties")).activate();
+			configProps = DSAdapter.readConfiguration("esp-rtdm-adapter.properties");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
 		
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		String engineUrl = configProps.getProperty("esp.url");
+		String rtdmHost = configProps.getProperty("rtdm.host");
+		int rtdmPort = Integer.parseInt(configProps.getProperty("rtdm.port"));
+		String rtdmEventName = configProps.getProperty("ESP_Twitter_Event");
+		Level logLevel = Level.parse(configProps.getProperty("tinylog.level").toUpperCase());
 		
 		
 		DSAdapterContext ctx = new DSAdapterContext();
-		ctx.setRtdmEngine(new RDMEngine(rtdmHost, rtdmPort, new Properties()));
+		ctx.setRtdmEngine(new RDMEngine(rtdmHost, rtdmPort, configProps));
 		ctx.setConfigProperties(new Properties());
 		ctx.setRtdmEventName(rtdmEventName);
 		
 		dfESPclientHandler handler = new dfESPclientHandler();
-		handler.init(Level.FINEST);
+		handler.init(logLevel);
 
 		
 		
 		String schemaUrl = engineUrl.substring(0, engineUrl.indexOf('?')) + "?get=schema";
 		Logger.info("Check ESP Url: {}", schemaUrl);
-		ArrayList<String> schemaVector = handler.queryMeta(schemaUrl);
-		if(schemaVector.size()==1) {
-			Logger.info("ESP Url is valid and has the schema {}", schemaVector.get(0));
+		ArrayList<String> schemaVector = null;
+		
+		try {
+			schemaVector = handler.queryMeta(schemaUrl);
+		} catch (UnknownHostException e) {
+			Logger.error(e);
+			return;
 		}
-		else {
+
+		if(schemaVector == null || schemaVector.size() != 1)  {
 			Logger.error("ESP Url is invalid. Please check the syntax and that ESP is running properly.");
+			return;
 		}
 		
 		
@@ -88,17 +84,25 @@ public class DSAdapter {
 		dfESPclient client = handler.subscriberStart(engineUrl, new DSAdapterCallbacks(), ctx);
 
 		/* Now make the actual connection to the ESP application or server. */
-		if (!handler.connect(client)) {
-			Logger.error("Start subscribing failed. Can not connect to ESP window");
-			System.exit(1);
+		try {
+			if (!handler.connect(client)) {
+				Logger.error("Start subscribing failed. Can not connect to ESP window");
+				System.exit(1);
+			}
+		} catch (SocketException | UnknownHostException e) {
+			Logger.error(e);
 		}
+		
+		
+		
+		
 		/* Create a mostly non-busy wait loop. */
 		while (nonBusyWait) {
 			try {
-				//Logger.info("Putting Thread to sleep for 1000ms");
+				Logger.debug("Putting Thread to sleep for 1000ms");
 				Thread.sleep(1000); // sleep for 1000 ms
 			} catch (InterruptedException ie) {
-				//Logger.info("Continue Thread to sleep for 1000ms");
+				Logger.debug("Continue Thread to sleep for 1000ms");
 			}
 		}
 
